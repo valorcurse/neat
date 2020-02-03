Added Xor demo Fixed bugs with not ordered adjacency matrix and returning the wrong results with phenotypes of different sizes

multiobjectivePopulation.py
@@ -22,16 +22,13 @@ from neat.speciatedPopulation import SpeciatedPopulation, SpeciesConfiguration, 
 
 class MOConfiguration(PopulationConfiguration):
     def __init__(self, population_size: int, n_inputs: int, n_outputs: int, behavior_dimensions: int, obj_ranges: List[Tuple[float, float]]):
-        self._data = {
-            "population_size": population_size,
-            "n_inputs": n_inputs,
-            "n_outputs": n_outputs,
-            "behavior_dimensions": behavior_dimensions,
-            "objective_ranges": obj_ranges
-        }
+        super().__init__(population_size, n_inputs, n_outputs)
+
+        self._data["behavior_dimensions"] = behavior_dimensions
+        self._data["objective_ranges"] = obj_ranges
 
 class MOUpdate(SpeciesUpdate):
-    def __init__(self, behaviors, fitness: np.ndarray):
+    def __init__(self, fitness: np.ndarray, behaviors):
         super().__init__(fitness)
 
         self._data["behaviors"] = behaviors
@@ -110,14 +107,10 @@ class MOPopulation(SpeciatedPopulation):
                "Only {}/{} genomes were copied.".format(len(nd_genomes), space_to_fill)
 
         self.genomes = nd_genomes
+        self.speciateAll()
+
         for s in self.species:
-            s.members = []
-
-        for g in self.genomes:
-            self.speciate(g)
-
-        for s in [s for s in self.species if len(s.members) == 0]:
-            self.species.remove(s)
+            s.num_of_elites = len(s.members)
 
         nd_indexes = [a for l in ndf[:i] for a in l]
         update.fitness = np.array([update.fitness[i] for i in nd_indexes], dtype=np.float32)

phenotypes.py
@@ -70,7 +70,9 @@ class Phenotype:
         self.in_nodes = [n for n in self.graph.nodes.data() if n[1]['type'] == NeuronType.INPUT]
         self.out_nodes = [n for n in self.graph.nodes.data() if n[1]['type'] == NeuronType.OUTPUT]
 
-        self.adjacency_matrix = nx.to_numpy_matrix(self.graph, dtype=np.float32)
+        # Adjacency matrix sorted topologically
+        self.adjacency_matrix = nx.adjacency_matrix(self.graph, nodelist=list(nx.topological_sort(self.graph))).todense()
+        # self.adjacency_matrix = nx.to_numpy_matrix(self.graph, dtype=np.float32)
 
         self.activations = np.array([neat.genes.FuncsEnum[self.graph.nodes()[n]['activation'].__name__].value for n in self.graph.nodes()], dtype=np.int32)
         self.bias = np.array([self.graph.nodes()[n]['bias'] for n in self.graph.nodes()], dtype=np.int32)
@@ -86,37 +88,33 @@ class FeedforwardCUDA(object):
     def __init__(self):
         self.threadsperblock = 32
 
-        self.mem = []
-
     def update(self, phenotypes, X):
 
         if type(X) is not np.ndarray:
             X = np.array(X)
 
-        self.blockspergrid = (len(phenotypes) + (self.threadsperblock - 1)) // self.threadsperblock
+        blockspergrid = (len(phenotypes) + (self.threadsperblock - 1)) // self.threadsperblock
 
-        self.phenotypes = phenotypes
+        num_of_outputs = len(phenotypes[0].out_nodes)
 
-        self.num_of_outputs = len(self.phenotypes[0].out_nodes)
-
-
-        assert X.shape[1] == len(phenotypes[0].in_nodes), \
+        assert X.shape == (len(phenotypes), len(phenotypes[0].in_nodes)), \
             "Incorrect number of input values. There are {} instead of {}: {}".format(
-                X.shape[1],
-                len(phenotypes[0].in_nodes),
+                X.shape,
+                (len(phenotypes), len(phenotypes[0].in_nodes)),
                 phenotypes[0].graph.nodes.data()
             )
 
 
 
-        adj_matrices = [p.adjacency_matrix for p in self.phenotypes]
+        adj_matrices = [p.adjacency_matrix for p in phenotypes]
         acts = [p.activations for p in phenotypes]
         bias = [p.bias for p in phenotypes]
 
         largest_adj_size = max([n.shape[0] for n in adj_matrices])
-
-        # Pad all matrices ti conform with the largest network
+        original_sizes = np.zeros(len(phenotypes), dtype=np.int32)
+        # Pad all matrices to conform with the largest network
         for i, (adj, act, bia) in enumerate(zip(adj_matrices, acts, bias)):
+            original_sizes[i] = adj.shape[0]
             new_size = largest_adj_size - adj.shape[0]
 
             adj_matrices[i] = np.pad(adj, [(0, new_size), (0, new_size)], mode='constant')
@@ -134,15 +132,16 @@ class FeedforwardCUDA(object):
             row[:X.shape[1]] = X[row_i]
 
 
-        results = np.array([np.zeros(self.num_of_outputs, dtype=np.float32) for _ in adj_matrices])
+        results = np.array([np.zeros(num_of_outputs, dtype=np.float32) for _ in adj_matrices])
 
         cuda_mem = cuda.to_device(mem)
         cuda_adj = cuda.to_device(adj_matrices)
         cuda_acts = cuda.to_device(acts)
         cuda_bias = cuda.to_device(bias)
+        cuda_original_sizes = cuda.to_device(original_sizes)
         cuda_results = cuda.to_device(results)
 
-        execute_network[self.blockspergrid, self.threadsperblock](cuda_mem, cuda_adj, cuda_acts, cuda_bias, cuda_results)
+        execute_network[blockspergrid, self.threadsperblock](cuda_mem, cuda_adj, cuda_acts, cuda_bias, cuda_original_sizes, cuda_results)
         results = cuda_results.copy_to_host()
 
         return results
@@ -253,6 +252,25 @@ def feedForward(adj, acts, bias, mem):
             mem[m_i] = sum  # Linear
 
 @cuda.jit()
+def execute_network(all_mem, all_adj, all_acts, all_bias, all_original_sizes, all_results):
+    i = cuda.blockIdx.x * cuda.blockDim.x + cuda.threadIdx.x
+    if i >= all_mem.shape[0]:
+        return
+
+    mem = all_mem[i]
+    adj = all_adj[i]
+    acts = all_acts[i]
+    bias = all_bias[i]
+    original_size = all_original_sizes[i]
+    results = all_results[i]
+
+    feedForward(adj, acts, bias, mem)
+
+    size_diff = results.shape[0] - original_size
+    for j in range(results.shape[0]):
+        results[j] = mem[-(results.shape[0] + size_diff) + j]
+
+@cuda.jit()
 def calc_substrate(X, Y, adj, acts, mems, results):
     i = cuda.blockIdx.x * cuda.blockDim.x + cuda.threadIdx.x
     if i >= results.shape[0]:
@@ -289,20 +307,3 @@ def calculateLinks(X, Y, start_index, i, mem):
     mem[1] = X[x][1]
     mem[2] = Y[y][0]
     mem[3] = Y[y][1]
-
-@cuda.jit()
-def execute_network(all_mem, all_adj, all_acts, all_bias, all_results):
-    i = cuda.blockIdx.x * cuda.blockDim.x + cuda.threadIdx.x
-    if i >= all_mem.shape[0]:
-        return
-
-    mem = all_mem[i]
-    adj = all_adj[i]
-    acts = all_acts[i]
-    bias = all_bias[i]
-    results = all_results[i]
-
-    feedForward(adj, acts, bias, mem)
-
-    for j in range(results.shape[0]):
-        results[j] = mem[-results.shape[0] + j]

speciatedPopulation.py
@@ -35,7 +35,8 @@ class Species:
         self.highestFitness: float = 0.0
         self.generationsWithoutImprovement: int = 0
 
-        # self.milestone: float = leader.milestone
+
+        self.num_of_elites = 2
 
         self.stagnant: bool = False
 
@@ -78,12 +79,7 @@ class Species:
 
 class SpeciesConfiguration(PopulationConfiguration):
     def __init__(self, population_size: int, n_inputs: int, n_outputs: int):
-        self._data = {
-            "population_size": population_size,
-            "n_inputs": n_inputs, 
-            "n_outputs": n_outputs
-        }
-
+        super().__init__(population_size, n_inputs, n_outputs)
 
 class SpeciesUpdate(PopulationUpdate):
     def __init__(self, fitness: np.ndarray):
@@ -113,7 +109,8 @@ class SpeciatedPopulation(Population):
         self.species.append(firstSpecies)
         
         for i in range(self.population_size - 1):
-            newGenome = fastCopy(baseGenome)
+            # newGenome = fastCopy(baseGenome)
+            newGenome = deepcopy(baseGenome)
             newGenome.ID = self.currentGenomeID
             firstSpecies.addMember(newGenome)
             self.genomes.append(newGenome)
@@ -151,11 +148,12 @@ class SpeciatedPopulation(Population):
             s.adjustFitnesses()
 
         # multiobjectivePopulation.py:86
-        pop_available = self.population_size - self.population_size/5
+        # pop_available = self.population_size - self.population_size/5
+        pop_available = self.population_size
         half_pop = int(pop_available / 2)
         shared_pop = int(half_pop / len(self.species))
 
-        debug_print("half_pop: {} | shared_pop: {}".format(half_pop, shared_pop))
+        # debug_print("half_pop: {} | shared_pop: {}".format(half_pop, shared_pop))
 
         species_fitnesses = [np.array([m.adjustedFitness for m in s.members]) for s in self.species]
         sum_of_fitnesses = [0.0]*len(species_fitnesses)
@@ -174,7 +172,37 @@ class SpeciatedPopulation(Population):
             spawn_portion = sum_of_fitnesses[i]/total_fitnesses
             s.numToSpawn = shared_pop + int(spawn_portion * half_pop)
 
-            debug_print("Species {} - Spawn: {}".format(s.ID, s.numToSpawn))
+            # debug_print("Species {} - Spawn: {}".format(s.ID, s.numToSpawn))
+
+    def speciateAll(self):
+        # Wipe species clean for redistrubition of genomes
+        for s in self.species:
+            s.members = []
+
+        # Distribute genomes to their most fitting species
+        for g in self.genomes:
+            self.speciate(g)
+
+        # Remove species that have no more members
+        self.purgeSpecies()
+
+        # Determine new species leader from the new members
+        for s in self.species:
+            distances = [m.calculateCompatibilityDistance(s.leader) for m in s.members]
+            new_leader_id = np.argmin(distances)
+            new_leader = s.members[new_leader_id]
+
+            # debug_print("Species {} - Distance between leaders: {}".format(s.ID, distances[new_leader_id]))
+
+            s.leader = new_leader
+            # possible_leaders.remove(s.leader)
+
+            # s.members = []
+
+        # self.purgeSpecies()
+
+        genomes_in_species = np.sum([len(s.members) for s in self.species])
+        assert genomes_in_species == len(self.genomes), "There are genomes that are not part of a species. {}/{} are accounted for.".format(genomes_in_species, len(self.genomes))
 
     def speciate(self, genome) -> None:
 
@@ -183,54 +211,57 @@ class SpeciatedPopulation(Population):
             random.choice(genome.parents).species.addMember(genome)
             return
 
-        species = None
-        for s in self.species:
-            distance = genome.calculateCompatibilityDistance(s.leader)
+        distances = [genome.calculateCompatibilityDistance(s.leader) for s in self.species]
+        closest_species = np.argmin(distances)
 
-            if distance <= self.mutationRates.newSpeciesTolerance:
-                species = s
-                break
-
-        if species is None:
+        # If it doesn't fit anywhere, create new species
+        if distances[closest_species] > self.mutationRates.newSpeciesTolerance:
             self.speciesNumber += 1
             self.species.append(Species(self.speciesNumber, genome))
         else:
-            species.addMember(genome)
+            self.species[closest_species].addMember(genome)
+
+    # Remove species without members
+    def purgeSpecies(self):
+        for s in [s for s in self.species if len(s.members) == 0]:
+            self.species.remove(s)
 
     def tournament_selection(self, genomes):
         return sorted(genomes, key=lambda x: x.fitness)[0]
 
     def newGeneration(self) -> None:
 
-        # for s in self.species:
-        #     s.leader = random.choice(s.members)
-            # s.members = []
 
-        newPop = []
+        new_genomes = []
         for s in self.species:
-            # s.members.sort(reverse=True, key=lambda x: x.fitness)
+            newPop = []
 
-            newPop.extend(s.members)
+            # reproduction_members = copy(s.members)
+            reproduction_members = []
 
-            reproduction_members = copy(s.members)
-
-            # topPercent = int(math.ceil(0.01 * len(s.members)))
+            s.members.sort(reverse=True, key=lambda x: x.fitness)
             # Grabbing the top 2 performing genomes
-            # for topMember in s.members[:topPercent]:
-            #     newPop.append(topMember)
-            #     s.numToSpawn -= 1
+            for topMember in s.members[:s.num_of_elites]:
+                newPop.append(topMember)
+                # reproduction_members.append(topMember)
+                s.numToSpawn -= 1
 
             # Only use the survival threshold fraction to use as parents for the next generation.
-            # cutoff = int(math.ceil(0.1 * len(s.members)))
             # Use at least two parents no matter what the threshold fraction result is.
-            # cutoff = max(cutoff, 2)
-            # s.members = s.members[:cutoff]
+            cutoff = max(int(math.ceil(0.1 * len(s.members))), 2)
+            reproduction_members = copy(s.members[:cutoff])
+            # for topMember in s.members[:cutoff]:
+            #     newPop.append(topMember)
+            #     reproduction_members.append(topMember)
+            # reproduction_members = s.members[:cutoff]
 
+            # s.members = []
 
             # Generate new baby genome
-            to_spawn = max(0, s.numToSpawn - len(s.members))
-            debug_print("Spawning {} for species {}".format(to_spawn, s.ID))
-            for i in range(to_spawn):
+            # to_spawn = max(0, s.numToSpawn - len(s.members))
+            # to_spawn = max(0, s.numToSpawn)
+            # debug_print("Spawning {} for species {}".format(to_spawn, s.ID))
+            for i in range(s.numToSpawn):
                 baby: genes.Genome = None
 
                 if random.random() > self.mutationRates.crossoverRate:
@@ -240,45 +271,31 @@ class SpeciatedPopulation(Population):
                 else:
 
                     # Tournament selection
-
-                    randomMembers = lambda: [random.choice(reproduction_members) for _ in range(5)]
+                    randomMembers = lambda: [random.choice(reproduction_members) for _ in range(2)]
                     g1 = self.tournament_selection(randomMembers())
                     g2 = self.tournament_selection(randomMembers())
-
-                    # g1 = sorted(randomMembers(), key=lambda x: x.fitness)[0]
-                    # g2 = sorted(randomMembers(), key=lambda x: x.fitness)[0]
 
                     baby = self.crossover(g1, g2)
 
                 self.currentGenomeID += 1
                 baby.ID = self.currentGenomeID
 
-                # Find species for new baby
-                # self.speciate(baby)
                 s.addMember(baby)
 
                 newPop.append(baby)
 
-        #  Remove empty species
-        for s in [s for s in self.species if len(s.members) == 0]:
-            self.species.remove(s)
+            s.members = newPop
+            new_genomes += s.members
 
-        self.genomes = newPop
+        self.genomes = new_genomes
 
     def reproduce(self) -> List[genes.Genome]:
-        for s in self.species:
-            s.leader = min([(m, m.calculateCompatibilityDistance(s.leader)) for m in s.members], key=lambda m: m[1])[0]
-            s.members = []
-            # for m in s.members:
-            #     self.speciate(m)
-
-
-        for g in self.genomes:
-            self.speciate(g)
-
         self.generation += 1
 
+        self.speciateAll()
+
         self.calculateSpawnAmount()
+
         self.newGeneration()
 
         return self.genomes